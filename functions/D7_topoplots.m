ddf29ff14b3aa9746fab3c21eb563e1935e86d48
Scyo-Victor <103148581+Scyo-Victor@users.noreply.github.com>
2022-04-30 18:20:20 +0200

    D6-D7 modifications

--- functions/D7_topoplots.m
+++ functions/D7_topoplots.m
@@ -2,8 +2,8 @@ function  D7_topoplots(grand_mean_matrix,locs)
 
 potentials=zeros(61,1);
 for ch=1:61
-    potentials(ch,1)=min(grand_mean_matrix(ch,2.4*512:2.6*512));
+    potentials(ch,1)=min(grand_mean_matrix(ch,round(2.4*512):round(2.6*512)));
 end
-
-topoplot(potentials,locs);
+topoplot(squeeze(potentials),locs,'maplimits',[-60 0]);
+colormap turbo;
 colorbar;
